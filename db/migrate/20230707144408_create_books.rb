2fb94073750018563eceda2958a0aebb6b77f321
mokkedano <126681793+mokkedano@users.noreply.github.com>
2023-07-11 14:26:26 +0000

    [Add] カラム追加

--- db/migrate/20230707144408_create_books.rb
+++ db/migrate/20230707144408_create_books.rb
@@ -1,6 +1,10 @@
 class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
+      
+      t.string :title, null: false
+      t.text :body
+      t.inteber :user_id
 
       t.timestamps
     end
