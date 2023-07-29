c42ae421cb8797b45d774aac6f92d18f3feb2506
mokkedano <126681793+mokkedano@users.noreply.github.com>
2023-07-29 14:49:43 +0000

    [Add] books

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "homes/about" => "homes#about", as: "about"
 
-  resources :books, only: [:index, :show]
+  resources :books, only: [:index, :show, :edit, :update, :destroy]
   resources :users, only: [:index, :show, :edit]
   
 end
