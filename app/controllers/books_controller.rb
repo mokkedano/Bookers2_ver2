541739a0df05e194394849785835fe5f3c981d6b
mokkedano <126681793+mokkedano@users.noreply.github.com>
2023-07-30 14:06:12 +0000

    [Add] books_controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,39 @@
 class BooksController < ApplicationController
   
   def index
+    @books = Book.all
   end
   
   
   def show
+    @book = Book.find(params[:id])
   end
+  
+  
+  def create
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  
+  def update
+  end
+  
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+  
+  
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
