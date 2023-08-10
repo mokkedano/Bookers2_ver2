46e1d80e44c85637d7b8d3508add556b8ec4dfb9
mokkedano <126681793+mokkedano@users.noreply.github.com>
2023-08-10 14:18:11 +0000

    [Add] users_edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  
-  
+
+
   def show
     @user = current_user
     @book = Book.new
@@ -17,16 +17,22 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-  
-  
+
+
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "You have updated user successfully."
+    else
+      render "edit"
+    end
   end
-  
-  
+
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
-  
+
 end
